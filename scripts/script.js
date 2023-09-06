7b7fc4b26e9c271dc66b884a3143f3df8454c839
Armando Rocha <armando.sv.rocha@gmail.com>
2023-09-06 09:01:07 -0300

    add sticky nav through js

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,22 @@ btn.addEventListener('click', (e) => {
 
   nav.classList.toggle('nav-open');
 })
+
+// Sticky nav
+const heroSection = document.querySelector('.section-hero');
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    const ent = entries[0];
+
+    if (ent.isIntersecting === false)
+      document.querySelector('.header').classList.add('sticky-nav');
+    else
+      document.querySelector('.header').classList.remove('sticky-nav');
+  },
+  {
+    root: null,
+    threshold: 0
+  });
+
+observer.observe(heroSection);
